0ac2d75611098c3be20bfe451c484d42a6956da7
cxj-ops <2776147265@qq.com>
2024-05-08 21:52:29 +0800

    My first Java program

---
+++ java_foundation/src/main/java/net/java/foundation/Helloworld.java
@@ -0,0 +1,12 @@
+package net.java.foundation;
+
+/**
+ * @author cxj-ops 2776147265@qq.com
+ * @version 2024/5/8 21:49
+ * @since JDK 11
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
